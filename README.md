a0bbc44d9b0e7d29054f0b96d24adf9ae00c49da
Konrad Zagozda <k.zagozda@lekseek.com>
2022-05-06 19:10:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Backend (Django) Developer - Images - recruitment task for hexocean
+
+
+## How to run?
+
+
+## Explanations
+
+It is not specified who could use the link - I assumed everyone should, so basically we're building a image sharing service.
+
+Users with Enterprise should have an ability to fetch a link that expires a number of seconds - each image upload for 
+any of tiers, returns set of links thus for simplification each of those links will have expiry time, technically this 
+point is met.
+
+It's good practice to override User model but, it's not necessary here, but tier belongs to user not the other way around.
+
+### Performance considerations
+
+We shouldn't compute thumbnails every time a request is made for it, probably the best idea is to use in-memory-database like redis.
---
+++ imagesharing/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# base model useful for logging changes
+class TimestampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+# reference with get_user_model()
+
+class User(TimestampedModel, AbstractUser):
+    tier = models.ForeignKey('Tier', on_delete=models.PROTECT) # make sure user belongs to other tier before deleting it.
+
+
+class ThumbnailSize(TimestampedModel):
+    height = models.IntegerField(help_text='height in pixels')
+
+
+class Tier(TimestampedModel):
+    thumbnail_sizes = models.ManyToManyField(ThumbnailSize)
+    expiring_links = models.BooleanField(help_text='ability to generate short-lived links', default=False)
+    original_image = models.BooleanField(help_text='ability to get original-size image', default=False)
+
+
+
